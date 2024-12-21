e23f5eb5091b4df328b514a3b58ca2b9754f4b70
federickDS <federicodistefano340@gmail.com>
2024-12-21 19:09:29 +0100

    JavaFX first try

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.example {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    // Consente a JavaFX di accedere al package principale
+    opens org.example.boundary to javafx.fxml;
+    // Specifica che questo modulo contiene il package principale
+    exports org.example.boundary;
+}
