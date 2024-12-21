fee55d607de5dccb657ebb0e96993d153d4550b0
federickDS <federicodistefano340@gmail.com>
2024-12-21 21:30:57 +0100

    nuovo tentativo con module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.example {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
 
     // Consente a JavaFX di accedere al package principale
     opens org.example.boundary to javafx.fxml;
