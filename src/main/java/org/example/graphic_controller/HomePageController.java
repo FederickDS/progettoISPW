73aaf4e03ee9f7d9f062e2a542d83cbbd4d118b9
federickDS <federicodistefano340@gmail.com>
2025-01-25 20:06:55 +0100

    risolto smell

--- src/main/java/org/example/graphic_controller/HomePageController.java
+++ src/main/java/org/example/graphic_controller/HomePageController.java
@@ -6,6 +6,7 @@ import org.example.view.HomePage;
 public class HomePageController {
     private final HomePage homePage;
     private final NavigationService navigationService;
+    private static String HOME_PAGE = "HomePage";
 
     public HomePageController(NavigationService navigationService) {
         this.navigationService = navigationService;
@@ -17,13 +18,13 @@ public class HomePageController {
 
     private void addEventHandlers() {
         // Gestione evento "Prenota Stanza"
-        homePage.getBookRoomButton().setOnAction(e -> navigationService.navigateToRoomBookingOptions(navigationService, "HomePage", "ServiceSelection"));
+        homePage.getBookRoomButton().setOnAction(e -> navigationService.navigateToRoomBookingOptions(navigationService, HOME_PAGE, "ServiceSelection"));
 
         // Effettua l'accesso (per area riservata)
-        homePage.getloginButton().setOnAction(e -> navigationService.navigateToLogin(navigationService, "HomePage", "HomePage", "Client"));
+        homePage.getloginButton().setOnAction(e -> navigationService.navigateToLogin(navigationService, HOME_PAGE, HOME_PAGE, "Client"));
 
         //gestione altri bottoni
-        homePage.getbookActivityButton().setOnAction(e->navigationService.navigateToNotImplemented(navigationService, "HomePage"));
+        homePage.getbookActivityButton().setOnAction(e->navigationService.navigateToNotImplemented(navigationService, HOME_PAGE));
     }
 
     public VBox getView(){
